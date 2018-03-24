c82d99ddb095fd8503275cbe3023b4212fb03b63
yotinny <36237707+yotinny@users.noreply.github.com>
2018-03-24 20:37:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,8 +10,17 @@ $events = json_decode($content, true);// Validate parsed JSON dataif
       $messages = [				'type' => 'text',				'text' => $text			];			// Make a POST Request to Messaging API to reply to sender			
       $url = 'https://api.line.me/v2/bot/message/reply';			
       $data = [				'replyToken' => $replyToken,				'messages' => [$messages],			];			
-      $post = json_encode($data);			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);			
-      $ch = curl_init($url);			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);			
-      $result = curl_exec($ch);			curl_close($ch);			echo $result . "";		}	
+      $post = json_encode($data);			
+      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);			
+      $ch = curl_init($url);			
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");			
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);			
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);			
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);			
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);			
+      $result = curl_exec($ch);			
+      curl_close($ch);			
+      echo $result . "";		}	
   }
-}echo "OK";
+}
+echo "OK";
