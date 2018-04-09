f2387d1900db0630ff51efd743c64808e4d520d9
yotinny <36237707+yotinny@users.noreply.github.com>
2018-04-10 05:13:51 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ if ( sizeof($request_array['events']) > 0 )
     $temx_tm = $text;
     if ($temx_tm == "Count" || $temx_tm == "Cr" || $temx_tm == "Ct" || $temx_tm == "Cc" || $temx_tm == "Cr" || $temx_tm == "Cor" || $temx_tm == "Cob" || $temx_tm == "Coy")
     {
-     $url = "https://api.thingspeak.com/channels/427743/fields/1/last.xml?results=500";
+     $url = "https://api.thingspeak.com/channels/427743/fields/1/last.xml?results=1";
      $xml = simplexml_load_file($url);
      $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้วนะจะ ('.$xml.')';
     }
