1ae530166e37fac9d8d8250d0e9d42c8056ae616
yotinny <36237707+yotinny@users.noreply.github.com>
2018-04-24 00:49:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -32,7 +32,7 @@ if ( sizeof($request_array['events']) > 0 )
      $url = "https://api.thingspeak.com/channels/427743/feeds.xml?results=1";
      $xml = simplexml_load_file($url);
      $field7 = $xml->xpath('//feed/field7');
-     $xmlelement = $xml->value('//feed/field7');
+     $xmlelement = $xml->value($field7);
      $re = print_r($field7, true);
      $img = base64_decode($re);
      $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้วนะจะ ('.$re.') + '.$xmlelement.'';
