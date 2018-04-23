b1792c40a0451adb21d4b36029cd621aa4296eb8
yotinny <36237707+yotinny@users.noreply.github.com>
2018-04-24 00:50:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -32,10 +32,10 @@ if ( sizeof($request_array['events']) > 0 )
      $url = "https://api.thingspeak.com/channels/427743/feeds.xml?results=1";
      $xml = simplexml_load_file($url);
      $field7 = $xml->xpath('//feed/field7');
-     $xmlelement = $xml->value($field7);
+     //$xmlelement = $xml->value($field7);
      $re = print_r($field7, true);
      $img = base64_decode($re);
-     $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้วนะจะ ('.$re.') + '.$xmlelement.'';
+     $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้วนะจะ ('.$re.') + '.$img.'';
     }
     else
      $reply_message = 'Manual Commanline: Count Rectangle[Cr], Count Triangle[Ct], Count Circle[Cc], Count Red[Cor], Count Blue[Cob], Count Yellow[Coy], Example : You want to count triangle use commanline "Ct" ';
