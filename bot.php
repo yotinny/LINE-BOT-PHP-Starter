bcdb6a66dc3cde8a93a8d12263249a7f97ba24c0
yotinny <36237707+yotinny@users.noreply.github.com>
2018-04-24 01:51:38 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -112,4 +112,27 @@ function send_reply_message($url, $post_header, $post_body,$temx_tm)
  return $result;
 }
 
+function objectsIntoArray($arrObjData, $arrSkipIndices = array())
+{
+    $arrData = array();
+    
+    // if input is object, convert into array
+    if (is_object($arrObjData)) {
+        $arrObjData = get_object_vars($arrObjData);
+    }
+    
+    if (is_array($arrObjData)) {
+        foreach ($arrObjData as $index => $value) {
+            if (is_object($value) || is_array($value)) {
+                $value = objectsIntoArray($value, $arrSkipIndices); // recursive call
+            }
+            if (in_array($index, $arrSkipIndices)) {
+                continue;
+            }
+            $arrData[$index] = $value;
+        }
+    }
+    return $arrData;
+}
+
 ?>
